bc1837dce22bd38839f6423dfd9c2fea8e8107d2
Smokinghalo8 <ethanfbuckner@gmail.com>
2025-02-03 21:32:14 -0500

    Created this branch
    Heyo added the animation and enemy frog. Combat is still scary difficult :,(

--- project/ENEMY_FROG/frog.gd
+++ project/ENEMY_FROG/frog.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimationSprite2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
-var attack_def = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -16,21 +15,16 @@ func _physics_process(delta: float) -> void:
 	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-		if (velocity.x > 0):
-			animated_sprite.scale.x = 1
-		if (velocity.x < 0):
-			animated_sprite.scale.x = -1
-		animated_sprite.play("hop")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite.play("idle")
-		
-	if Input.is_action_just_pressed("attack"):
-		animated_sprite.play("attack")
-		
-		
+	#var direction := Input.get_axis("move_left", "move_right")
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#	if (velocity.x > 0):
+	#		animated_sprite.scale.x = 1
+	#	if (velocity.x < 0):
+	#		animated_sprite.scale.x = -1
+	#	animated_sprite.play("hop")
+	#else:
+	#velocity.x = move_toward(velocity.x, 0, SPEED)
+	animated_sprite.play("idle")
 
 	move_and_slide()
